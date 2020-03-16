9f8e5016c65f1f170ddbc456d2f6bb068f7c4fdf
tattommasi <47092404+tattommasi@users.noreply.github.com>
2020-03-16 13:59:03 +0100

    soluzione proposta

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
@@ -1,24 +1,34 @@
 package it.polito.tdp.parole.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Parole {
 		
+	List<String> lista;
+	
 	public Parole() {
-		//TODO
+		lista = new LinkedList<String>();
+		//lista = new ArrayList<String>();
 	}
 	
 	public void addParola(String p) {
-		//TODO
+		lista.add(p);
 	}
 	
 	public List<String> getElenco() {
-		//TODO
-		return null;
+		Collections.sort(lista);
+		return lista;
 	}
 	
 	public void reset() {
-		// TODO
+		lista.clear();
+	}
+	
+	public void removeParola(String string) {
+		lista.remove(string);
 	}
 
 }
